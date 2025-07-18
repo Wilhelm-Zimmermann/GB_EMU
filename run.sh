60fdee489184b0b5c1ab805ca40b2e9137217e1b
Wilhelm-Zimmermann <wilhelmzimmermann8@gmail.com>
2025-07-18 18:39:13 -0300

    feat: cpu return T-STATE

--- run.sh
+++ run.sh
@@ -3,4 +3,7 @@ cd build
 cmake ..
 make clean
 make
-./main.out "../ROMS/Legend of Zelda, The - Link's Awakening (USA, Europe) (Rev 2).gb"
+# ./main.out "../ROMS/Legend of Zelda, The - Link's Awakening (USA, Europe) (Rev 2).gb"
+# ./main.out "../ROMS/Legend of Zelda, The - Link's Awakening (USA, Europe) (Rev 2).gb"
+# ./main.out "../ROMS/04-op r,imm.gb.gb"
+./main.out "../ROMS/01-special.gb"
--- src/headers/prefixedInstructions.h
+++ src/headers/prefixedInstructions.h
@@ -3,20 +3,20 @@
 #include "./register.h"
 #include "./memory.h"
 
-void execPrefix(Register *reg, Memory *mem, uint8_t opcode);
-void opcode_xp0(Register *reg, Memory *mem,uint8_t opcode);
-void opcode_xp1(Register *reg, Memory *mem,uint8_t opcode);
-void opcode_xp2(Register *reg, Memory *mem,uint8_t opcode);
-void opcode_xp3(Register *reg, Memory *mem,uint8_t opcode);
-void opcode_xp4(Register *reg, Memory *mem,uint8_t opcode);
-void opcode_xp5(Register *reg, Memory *mem,uint8_t opcode);
-void opcode_xp6(Register *reg, Memory *mem,uint8_t opcode);
-void opcode_xp7(Register *reg, Memory *mem,uint8_t opcode);
-void opcode_xp8(Register *reg, Memory *mem,uint8_t opcode);
-void opcode_xp9(Register *reg, Memory *mem,uint8_t opcode);
-void opcode_xpA(Register *reg, Memory *mem,uint8_t opcode);
-void opcode_xpB(Register *reg, Memory *mem,uint8_t opcode);
-void opcode_xpC(Register *reg, Memory *mem,uint8_t opcode);
-void opcode_xpD(Register *reg, Memory *mem,uint8_t opcode);
-void opcode_xpE(Register *reg, Memory *mem,uint8_t opcode);
-void opcode_xpF(Register *reg, Memory *mem,uint8_t opcode);
+uint8_t execPrefix(Register *reg, Memory *mem, uint8_t opcode);
+uint8_t opcode_xp0(Register *reg, Memory *mem,uint8_t opcode);
+uint8_t opcode_xp1(Register *reg, Memory *mem,uint8_t opcode);
+uint8_t opcode_xp2(Register *reg, Memory *mem,uint8_t opcode);
+uint8_t opcode_xp3(Register *reg, Memory *mem,uint8_t opcode);
+uint8_t opcode_xp4(Register *reg, Memory *mem,uint8_t opcode);
+uint8_t opcode_xp5(Register *reg, Memory *mem,uint8_t opcode);
+uint8_t opcode_xp6(Register *reg, Memory *mem,uint8_t opcode);
+uint8_t opcode_xp7(Register *reg, Memory *mem,uint8_t opcode);
+uint8_t opcode_xp8(Register *reg, Memory *mem,uint8_t opcode);
+uint8_t opcode_xp9(Register *reg, Memory *mem,uint8_t opcode);
+uint8_t opcode_xpA(Register *reg, Memory *mem,uint8_t opcode);
+uint8_t opcode_xpB(Register *reg, Memory *mem,uint8_t opcode);
+uint8_t opcode_xpC(Register *reg, Memory *mem,uint8_t opcode);
+uint8_t opcode_xpD(Register *reg, Memory *mem,uint8_t opcode);
+uint8_t opcode_xpE(Register *reg, Memory *mem,uint8_t opcode);
+uint8_t opcode_xpF(Register *reg, Memory *mem,uint8_t opcode);

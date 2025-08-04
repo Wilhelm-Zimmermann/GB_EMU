a4045deba90455854adca5e6fe79cca5832208ee
Wilhelm-Zimmermann <wilhelmzimmermann8@gmail.com>
2025-08-04 18:08:17 -0300

    feat: ppu skeleton

--- src/headers/ppu.h
+++ src/headers/ppu.h
@@ -1,5 +1,23 @@
 #pragma once
+#include <stdint.h>
+#include <stdbool.h>
 
-typedef struct 
+typedef struct
 {
+    uint8_t lcdc; 
+    uint8_t stat; 
+    uint8_t scy;  
+    uint8_t scx;  
+    uint8_t ly;   
+    uint8_t lyc;  
+    uint8_t bgp;  
+    uint8_t obp0; 
+    uint8_t obp1; 
+    uint8_t wy;   
+    uint8_t wx;   
+
+    int modeClock;
 } PPU;
+
+void ppu_init();
+void ppu_step(int cpuCycles);
